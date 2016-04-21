740f8f08d3663e63641998b1ad09c508cdb34884
sylxiaozhi <sylxiaozhi@gmail.com>
2016-04-21 13:18:05 +0800

    I2C

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -51,20 +51,22 @@ void initSPI1();
 void setVoltage(char channel, char voltage);
 void MakeSinWave();
 void MakeTriWave();
+void i2c_master_setup();
+void initExpander();
 
 int main() {
-
+    
     __builtin_disable_interrupts();
-
+    
     // set the CP0 CONFIG register to indicate that kseg0 is cacheable (0x3)
     __builtin_mtc0(_CP0_CONFIG, _CP0_CONFIG_SELECT, 0xa4210583);
-
+    
     // 0 data RAM access wait states
     BMXCONbits.BMXWSDRM = 0x0;
-
+    
     // enable multi vector interrupts
     INTCONbits.MVEC = 0x1;
-
+    
     // disable JTAG to get pins back
     DDPCONbits.JTAGEN = 0;
     
@@ -78,14 +80,14 @@ int main() {
     MakeSinWave();
     MakeTriWave();
     initSPI1();
-     
-       
+    i2c_master_setup();
+    initExpander();
+    
     while(1){
         _CP0_SET_COUNT(0);
-        LATAINV = 0x10; // make sure timer2 works
         static int count1 = 0;
         static int count2 = 0;
-        setVoltage(0,SineWaveform[count1]); 
+        setVoltage(0,SineWaveform[count1]);
         setVoltage(1,TriWaveform[count2]);
         count1++;
         count2++;
@@ -95,25 +97,26 @@ int main() {
         if(count2 >= TriCount){
             count2 = 0;
         }
-        while(_CP0_GET_COUNT() < 24000) { 
+        while(_CP0_GET_COUNT() < 24000) {
             ;
+        }
+        if ((getExpander() & 0b10000000) == 0b10000000) {
+            setExpander(0b00000001,1);
+        }
+        else {
+            setExpander(0b00000001,0);                            
         }
     }
 }
 
 void initSPI1(){
-    // set up the chip select pin as an output
-    // the chip select pin is used by the MCP4902DAC to indicate
-    // when a command is beginning (clear CS to low) and when it
-    // is ending (set CS high)
     TRISBbits.TRISB7 = 0b0;
     CS = 1;
-    SS1Rbits.SS1R = 0b0100;   // assign SS1 to RB7
-    SDI1Rbits.SDI1R = 0b0000; // assign SDI1 to RA1 
-    RPB8Rbits.RPB8R = 0b0011; // assign SDO1 to RB8
-    ANSELBbits.ANSB14 = 0;    // turn off AN10
+    SS1Rbits.SS1R = 0b0100;
+    SDI1Rbits.SDI1R = 0b0000;
+    RPB8Rbits.RPB8R = 0b0011;
+    ANSELBbits.ANSB14 = 0;
     
-    // setup SPI1
     SPI1CON = 0;              // turn off the SPI1 module and reset it
     SPI1BUF;                  // clear the rx buffer by reading from it
     SPI1BRG = 0x1;            // baud rate to 12 MHz [SPI4BRG = (48000000/(2*desired))-1]
@@ -128,23 +131,23 @@ void initSPI1(){
 char SPI1_IO(char write){
     SPI1BUF = write;
     while(!SPI1STATbits.SPIRBF) { // wait to receive the byte
-     ;
-     }
-    return SPI1BUF; 
+        ;
+    }
+    return SPI1BUF;
 }
 
 void setVoltage(char channel, char voltage){
-    if(channel == 0) { // 0 for VoutA
-        CS = 0; 
-        SPI1_IO((voltage >> 4) | 0b01110000); // 4 configuration bits
-        SPI1_IO(voltage << 4); // Data bits 
-        CS = 1;   
+    if(channel == 0) { // VoutA
+        CS = 0;
+        SPI1_IO((voltage >> 4) | 0b01110000);
+        SPI1_IO(voltage << 4);
+        CS = 1;
     }
-    if(channel == 1) { // 1 for VoutB
-        CS = 0; 
-        SPI1_IO((voltage >> 4) | 0b11110000); // 4 configuration bits
-        SPI1_IO(voltage << 4); // Data bits
-        CS = 1;   
+    if(channel == 1) { // VoutB
+        CS = 0;
+        SPI1_IO((voltage >> 4) | 0b11110000);
+        SPI1_IO(voltage << 4);
+        CS = 1;
     }
 }
 
@@ -152,14 +155,94 @@ void MakeSinWave(){
     int i;
     for(i = 0; i < SineCount; i++){
         SineWaveform[i] = 127+128*sin(2*PI*i*0.01);
-        }
+    }
 }
 
 
 void MakeTriWave(){
     int j;
     for(j = 0; j < TriCount; j++){
-        TriWaveform[j] = 255*(j*0.005); 
+        TriWaveform[j] = 255*(j*0.005);
     }
 }
 
+void i2c_master_start(void) {
+    I2C2CONbits.SEN = 1;
+    while(I2C2CONbits.SEN) {
+        ;
+    }
+}
+
+void i2c_master_restart(void) {
+    I2C2CONbits.RSEN = 1;
+    while(I2C2CONbits.RSEN) {
+        ;
+    }
+}
+
+void i2c_master_send(unsigned char byte) {
+    I2C2TRN = byte;
+    while(I2C2STATbits.TRSTAT) {
+        ;
+    }
+    if(I2C2STATbits.ACKSTAT) {
+    }
+}
+
+unsigned char i2c_master_recv(void) {
+    I2C2CONbits.RCEN = 1;
+    while(!I2C2STATbits.RBF) {
+        ;
+    }
+    return I2C2RCV;
+}
+
+void i2c_master_ack(int val) {
+    I2C2CONbits.ACKDT = val;
+    I2C2CONbits.ACKEN = 1;
+    while(I2C2CONbits.ACKEN) {
+        ;
+    }
+}
+
+void i2c_master_stop(void) {
+    I2C2CONbits.PEN = 1;
+    while(I2C2CONbits.PEN) {
+        ;
+    }
+}
+
+void initExpander() {
+    
+    i2c_master_start();
+    i2c_master_send(0b01000000);
+    i2c_master_send(0x0A);
+    i2c_master_send(0b00000000);
+    i2c_master_stop();
+    
+}
+
+
+void setExpander(char pin, char level) {
+    char levelbyte = 0b11111111;
+    if (level == 0) {
+        levelbyte = 0b00000000;
+    }
+    i2c_master_start();
+    i2c_master_send(0b01000000);
+    i2c_master_send(0x0A);
+    i2c_master_send(pin & levelbyte);
+    i2c_master_stop();
+}
+
+void getExpander() {
+    i2c_master_start();
+    i2c_master_send(0b01000000);
+    i2c_master_send(0x09);
+    i2c_master_restart();
+    i2c_master_send(0b01000001);
+    char r = i2c_master_recv();
+    i2c_master_ack(1);
+    i2c_master_stop();
+    return r;
+}
