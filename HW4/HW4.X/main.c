e248942d75f837735506529360e2f9d035814618
sylxiaozhi <sylxiaozhi@gmail.com>
2016-04-21 13:28:17 +0800

    Final Commit

--- HW4/HW4.X/main.c
+++ HW4/HW4.X/main.c
@@ -241,8 +241,21 @@ void getExpander() {
     i2c_master_send(0x09);
     i2c_master_restart();
     i2c_master_send(0b01000001);
-    char r = i2c_master_recv();
+    char result = i2c_master_recv();
     i2c_master_ack(1);
     i2c_master_stop();
-    return r;
+    return result;
+}
+
+
+void i2c_master_setup(void) {
+    ANSELBbits.ANSB2 = 0;
+    ANSELBbits.ANSB3 = 0;
+    I2C2BRG = 233;
+    I2C2CONbits.ON = 1;
+    i2c_master_start();
+    i2c_master_send(0b01000000);
+    i2c_master_send(0x00);
+    i2c_master_send(0b11110000);
+    i2c_master_stop();
 }
